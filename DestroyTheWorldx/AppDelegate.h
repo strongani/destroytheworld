e6ae23ecc843bef15a0c596f831f1c24e77a7482
Vikram <vikram@vikrams-mbp.hsd1.ca.comcast.net>
2017-06-22 13:08:20 -0700

    Initial Commit

---
+++ DestroyTheWorldx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DestroyTheWorldx
+//
+//  Created by Vikram on 6/22/17.
+//  Copyright © 2017 Vikram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DestroyTheWorldx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DestroyTheWorldx
+//
+//  Created by Vikram on 6/22/17.
+//  Copyright © 2017 Vikram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
